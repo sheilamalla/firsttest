b496e16e9b4509db4076a2c8cf4dee7c25ca9461
sheilamalla <35785939+sheilamalla@users.noreply.github.com>
2018-03-20 14:27:49 -0400

    testing for modification

--- src/PracticeArray.java
+++ src/PracticeArray.java
@@ -2,7 +2,7 @@
 public class PracticeArray {
 
 	public static void main(String[] args) {
-		int[] array = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+		int[] array = { 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 35, 40 };
 		System.out.println(array.length);
 	}
 }
